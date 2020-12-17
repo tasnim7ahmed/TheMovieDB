2496d94cfc1479143a58b1577ba57e47e4fb4117
Tasnim Ahmed <tasnimahmed@iut-dhaka.edu>
2020-12-18 01:01:54 +0600

    Added Pagination

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import Navbar from "./components/Navbar";
 import Searchbar from "./components/Searchbar";
 import MovieList from "./components/MovieList";
 import { getMoviesByTerm } from "./api/TMDB";
+import Pagination from "./components/Pagination";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    await getMoviesByTerm(searchTerm, setMovies);
+    await getMoviesByTerm(searchTerm, setMovies, currentPage, setTotalPages);
   };
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const nextPage = async (page_number) => {
+    setCurrentPage(page_number);
+    await getMoviesByTerm(searchTerm, setMovies, currentPage, setTotalPages);
   };
 
   return (
@@ -22,6 +30,15 @@ const App = () => {
       <Navbar />
       <Searchbar handleChange={handleChange} handleSubmit={handleSubmit} />
       <MovieList movies={movies} />
+      {totalPages > 1 ? (
+        <Pagination
+          nextPage={nextPage}
+          currentPage={currentPage}
+          totalPages={totalPages}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
--- src/api/TMDB.js
+++ src/api/TMDB.js
@@ -4,14 +4,17 @@ const TMDB = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
 });
 
-const getMoviesByTerm = (SearchTerm, setMovies) => {
+const getMoviesByTerm = (SearchTerm, setMovies, page_number, setTotalPages) => {
   TMDB.get("/search/movie/", {
     params: {
       api_key: "209d31e2b7002857fcc0fdeff8329ae2",
       query: SearchTerm,
+      page: page_number,
     },
   }).then((response) => {
+    console.log(response.data);
     setMovies(response.data.results);
+    setTotalPages(response.data.total_pages);
   });
 };
 
